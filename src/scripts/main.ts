24c5451c16a68e28c5d42db4df0d303356baea7b
Adam Johannesmeyer <adamj0101@gmail.com>
2016-10-18 12:43:02 -0700

    feature() add rxjs

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,10 @@
 require('../stylesheets/main.scss');
 require('../container/index.ts');
 
+
+// adds an 13kb to initial size
+const Observable = require('rxjs/Observable').Observable;
+require('rxjs/add/operator/map');
+require('rxjs/add/observable/fromEvent');
+
 document.write('main ts file');
