23943a90f70cd3a83ba5b36819445b6c9de76aa8
Adam Johannesmeyer <adamj0101@gmail.com>
2016-10-18 18:56:10 -0700

    setup for rxjs and regular manipulation of css variables

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,10 +1,26 @@
-require('../stylesheets/main.scss');
-require('../container/index.ts');
-
+import '../stylesheets/main.scss';
+import '../container/index.ts';
 
 // adds an 13kb to initial size
-const Observable = require('rxjs/Observable').Observable;
-require('rxjs/add/operator/map');
-require('rxjs/add/observable/fromEvent');
+// import { Observable } from 'rxjs/Observable';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/observable/fromEvent';
+
+// const docStyle = document.documentElement.style;
+// console.log(yeup)
+
+// const mouseMove$ = Observable
+//   .fromEvent(window, 'mousemove');
+
+// mouseMove$.subscribe(({x, y}) => {
+//   docStyle.setProperty('--space', y+'px');
+// });
+
+const docStyle = document.documentElement.style;
+const el = document.getElementById("nope");
+el.addEventListener("mousemove", function({x, y}) {
+  docStyle.setProperty('--space', y+'px');
+});
 
 document.write('main ts file');
+
