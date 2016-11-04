123c6d31079b089b949e66d44a1521fd93b2cc49
Adam Johannesmeyer <adamj0101@gmail.com>
2016-11-04 15:36:17 -0700

    fix() add clientX and clientY for Mozilla

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -6,13 +6,17 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/fromEvent';
 
+
+
 function moveShadow() {
   const docStyleRx = document.documentElement.style;
 
   const mouseMove$ = Observable
     .fromEvent(window, 'mousemove');
 
-  mouseMove$.subscribe(({x, y}) => {
+  mouseMove$.subscribe(({clientX, clientY}) => {
+    let x = clientX;
+    let y = clientY;
     let height = window.innerHeight;
     let width = window.innerWidth;
 
@@ -23,22 +27,60 @@ function moveShadow() {
       calcX = ((calcX) * -1 + 10);
     } else {
       calcX = (calcX - 10) * -1;
-    }
+    };
 
     if (calcY > 10.5) {
       calcY = ((calcY) * -1 + 10);
     } else {
       calcY = (calcY - 10) * -1;
-    }
+    };
 
     if (isNaN(calcX) || isNaN(calcY)) {
       calcX = 0;
       calcY = 0;
-    }
+    };
 
     docStyleRx.setProperty('--x', calcX+'px');
     docStyleRx.setProperty('--y', calcY+'px');
   });
 }
 
-moveShadow();
+document.addEventListener('DOMContentLoaded', function() {
+    // mozillaFix();
+    moveShadow();
+}, false);
+
+// function nativeDom() {
+//   const docStyle = document.documentElement.style;
+//   const el = document.getElementById("nope");
+//   el.addEventListener("mousemove", (element) => {
+//       let x = element.clientX;
+//       let y = element.clientY;
+
+//       let height = window.innerHeight;
+//       let width = window.innerWidth;
+
+//       let calcX = (19 + (width / x)) / (width / x);
+//       let calcY = (19 + (height / y)) / (height / y);
+
+//       if (calcX > 10.5) {
+//         calcX = ((calcX) * -1 + 10);
+//       } else {
+//         calcX = (calcX - 10) * -1;
+//       }
+
+//       if (calcY > 10.5) {
+//         calcY = ((calcY) * -1 + 10);
+//       } else {
+//         calcY = (calcY - 10) * -1;
+//       }
+
+//       if (isNaN(calcX) || isNaN(calcY)) {
+//         calcX = 0;
+//         calcY = 0;
+//       }
+
+//       docStyle.setProperty('--x', calcX+'px');
+//       docStyle.setProperty('--y', calcY+'px');
+//   });
+// }
