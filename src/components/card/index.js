fa0155482dec76c0cd177dbe1e2d18105b6480a6
Adam Johannesmeyer <adamj0101@gmail.com>
2016-12-09 13:51:28 -0800

    refactor(card) use translate + svg for shadow instead of box-shadow
    Performance
    use svg element so I can animate it via `transform: translate(x,y)` and
    not `box-shadow`
    ✔ transform: translate(x,y) =  compositor thread only
    X box-shadow: x y size color = layout, painted, compositor thread

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -5,7 +5,9 @@ import Contact from '../contact';
 import CurrentProject from '../current-project';
 
 export default class Card extends Component {
+
 	cardStyle = {
+		
 		display: 'flex',
 		flexDirection: 'row',
 		justifyContent: 'center',
@@ -13,8 +15,8 @@ export default class Card extends Component {
 	}
 
 	cardInfo = {
-		boxShadow: "var(--x) var(--y) 5px rgba(100, 100, 100, .4)",
-
+		
+		
 		margin: 'auto',
 		minHeight: '250px',
 		minWidth: '400px',
@@ -24,41 +26,57 @@ export default class Card extends Component {
 		justifyContent: 'center',
 		flexWrap: 'wrap',
 
-		textAlign: 'center'
+		textAlign: 'center',
+
+		position: 'relative',
+		zIndex: '50'
+	}
+
+	mySvg = {
+		minHeight: '380px',
+		minWidth: '410px',
+		top: '-5px',
+		left: '-5px',
+
+		position: 'absolute',
+		zIndex: '-5',
 	}
 	
 	componentDidMount() {
-		const docStyle = document.documentElement.style;
+
+		// use svg element so I can animate it via `transform: translate(x,y)` and not `box-shadow`
+		// ✔ transform: translate(x,y) =  compositor thread only
+		// X box-shadow: x y size color = layout, painted, compositor thread
 		const el = document.getElementById("move-shadow");
-		el.addEventListener("mousemove", (element) => {
-				let x = element.clientX;
-				let y = element.clientY;
+		const svgElement = document.getElementById("svgElement"); 
+		el.addEventListener('mousemove', (element) => {
+			let x = element.clientX;
+			let y = element.clientY;
 
-				let height = window.innerHeight;
-				let width = window.innerWidth;
+			let height = window.innerHeight;
+			let width = window.innerWidth;
 
-				let calcX = (19 + (width / x)) / (width / x);
-				let calcY = (19 + (height / y)) / (height / y);
+			let calcX = (19 + (width / x)) / (width / x);
+			let calcY = (19 + (height / y)) / (height / y);
 
-				if (calcX > 10.5) {
-					calcX = ((calcX) * -1 + 10);
-				} else {
-					calcX = (calcX - 10) * -1;
-				}
+			if (calcX > 10.5) {
+				calcX = ((calcX) * -1 + 10);
+			} else {
+				calcX = (calcX - 10) * -1;
+			}
 
-				if (calcY > 10.5) {
-					calcY = ((calcY) * -1 + 10);
-				} else {
-					calcY = (calcY - 10) * -1;
-				}
+			if (calcY > 10.5) {
+				calcY = ((calcY) * -1 + 10);
+			} else {
+				calcY = (calcY - 10) * -1;
+			}
 
-				if (isNaN(calcX) || isNaN(calcY)) {
-					calcX = 0;
-					calcY = 0;
-				}
+			if (isNaN(calcX) || isNaN(calcY)) {
+				calcX = 0;
+				calcY = 0;
+			}
 
-				docStyle.setProperty('--x', calcX+'px');
-				docStyle.setProperty('--y', calcY+'px');
+			svgElement.style.transform = `translate(${calcX}px, ${calcY}px)`
 		})
 	}
 
@@ -66,10 +84,19 @@ export default class Card extends Component {
 		return (
       <div id="move-shadow" style={this.cardStyle}>
       <div style={this.cardInfo}>
-        <Contact/>
-        <CurrentProject/>
+				<svg id="svgElement" style={this.mySvg} xmlns="http://www.w3.org/2000/svg" version="1.1">
+					<defs>
+						<filter id="blur-2">
+							<feGaussianBlur in="SourceGraphic" stdDeviation="2"/>
+						</filter>
+					</defs>
+					<rect filter="url(#blur-2)" id="Rectangle" stroke="none" fill="rgba(100, 100, 100, .4)" fill-rule="evenodd" x="5" y="5" width="400" height="373"/>
+				</svg>
+				<Contact/>
+				<CurrentProject/>
       </div>
       </div>
+
 		);
 	}
 }
