f5d1fd71eb7f64326b3fc0ba06d15efbeaddebac
Adam Johannesmeyer <adamj0101@gmail.com>
2017-02-09 18:21:40 -0800

    config() add more uglify options

--- config/uglify.js
+++ config/uglify.js
@@ -3,14 +3,27 @@ module.exports = {
 		comments: 0
 	},
 	compress: {
-		unused: 1,
-		warnings: 0,
-		comparisons: 1,
-		conditionals: 1,
-		negate_iife: 0, // <- for `LazyParseWebpackPlugin()`
-		dead_code: 1,
-		if_return: 1,
-		join_vars: 1,
-		evaluate: 1
+    unsafe_comps: true,
+    properties: true,
+    keep_fargs: false,
+    pure_getters: true,
+    collapse_vars: true,
+    unsafe: true,
+    warnings: false,
+    screw_ie8: true,
+    sequences: true,
+    dead_code: true,
+    drop_debugger: true,
+    comparisons: true,
+    conditionals: true,
+    evaluate: true,
+    booleans: true,
+    loops: true,
+    unused: true,
+    hoist_funs: true,
+    if_return: true,
+    join_vars: true,
+    cascade: true,
+    drop_console: true
 	}
 }
