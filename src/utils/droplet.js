936f3099c8259c78d35489b80a99f9de15bd2d8a
AdamJo <adamj0101@gmail.com>
2017-03-02 22:11:20 -0800

    clean() removed unused code and better comments

--- src/utils/droplet.js
+++ src/utils/droplet.js
@@ -1,72 +1,55 @@
-export default class Droplet {
+import { _random, _mapConvert } from './helpers';
 
-  /*
-    @params
-      width  {number} = window width
-      height {number} = window height
-  */
+export default class Droplet {
+  /**
+   * @param  {number} width  - window width
+   * @param  {number} height - window height
+   */
   constructor(width, height) {
     this.firstFall = true; // determines if drops have hit bottom yet
     this.height = height; // height of screen
     this.width = width; // width of screen
-    this.color = this._random(20, 80); // random color to start with
+    this.color = _random(20, 80); // random color to start with
     this.lightness = 80; // default lightness setting
     this.lightnessDirection = 20; // amount of lightness change per color rotation
-    // this.startingPosition = this._random(0, 10) > 5 ? true : false; // start at bottom or top
-    this.x = this._random(0, this.width); // width on screen that drop starts at
-    // this.y = this.startingPosition
-    //   ? this._random(-350, -50)
-    //   : this._random(this.height + 50, this.height + 250); // height drop starts at
-    this.y = this._random(-350, -200);
-    this.z = this._random(0, 20); // how far or away a droplet is from screen
-    this.len = this._mapConvert(this.z, 0, 20, 10, 20); // determines length of droplet
-    this.yspeed = this._mapConvert(this.z, 0, 20, 4, 10); // speed at which drop falls
-    this.thickness = this._mapConvert(this.z, 0, 20, 1, 3); // determines thickness of 
+    this.x = _random(0, this.width); // width on screen that drop starts at
+    this.y = _random(-350, -200); // height drop starts at
+    this.z = _random(0, 20); // how far or away a droplet is from screen
+    this.len = _mapConvert(this.z, 0, 20, 10, 20); // determines length of droplet
+    this.yspeed = _mapConvert(this.z, 0, 20, 4, 10); // speed at which drop falls
+    this.thickness = _mapConvert(this.z, 0, 20, 1, 3); // determines thickness of
   }
 
-  // calculates the fall speed and resets droplet
+  
   fall() {
-
     // adds updated position to speed.
-    // this.y += this.startingPosition
-    //   ? this.yspeed
-    //   : this.yspeed * (-1);
     this.y += this.yspeed;
 
     // add grav for more variation in speed when droplets are falling
-    let grav = this._mapConvert(this.z, 0, 20, 0, 0.1);
+    let grav = _mapConvert(this.z, 0, 20, 0, 0.1);
     this.yspeed += grav;
 
     // for droplets coming from top to bottom
     if (this.y > this.height + 40) {
-      this.y = this._random(-250, -50);
-      this.x = this._random(0, this.width);
-      this.thickness = this._mapConvert(this.z, 0, 20, 1, 3);
-      this.yspeed = this._mapConvert(this.z, 0, 20, 4, 10);
+      this.y = _random(-250, -50);
+      this.x = _random(0, this.width);
+      this.thickness = _mapConvert(this.z, 0, 20, 1, 3);
+      this.yspeed = _mapConvert(this.z, 0, 20, 4, 10);
     }
-
-    // for droplets coming from bottom to top
-    // if (0 > this.y && !this.startingPosition) {
-    //   this.y = this._random(this.height + 50, this.height + 250); // height drop starts at
-    //   this.x = this._random(0, this.width);
-    //   this.thickness = this._mapConvert(this.z, 0, 20, 1, 3);
-    //   this.yspeed = this._mapConvert(this.z, 0, 20, 4, 10);
-    // }
   }
 
-  /*
-    Creates an object for stroke and color values of the canvas
-    @return {Object} = {
-      x:        {number} = x position of droplet
-      y:        {number} = y position of droplet
-      len:      {number} = length of droplet
-      thickness {number} = creates a parallax effect on droplet
-      color     {number} = cor hsl stroke color
-      lightness {number} = for hsl stroke lightness;
-    }
-  */
+  /**
+   *  Creates an object for stroke and color values of the canvas
+   * @return {Object} = {
+   *   x:        {number} = x position of droplet
+   *   y:        {number} = y position of droplet
+   *   len:      {number} = length of droplet
+   *   thickness {number} = creates a parallax effect on droplet
+   *   color     {number} = cor hsl stroke color
+   *   lightness {number} = for hsl stroke lightness;
+   * }
+   */
   show() {
-
     // when color reaches the end of the color scheme
     // reset color and modify lightness
     if (this.color > 360) {
--- src/utils/rain.js
+++ src/utils/rain.js
@@ -1,4 +1,4 @@
-import Droplet from './droplet.js'
+import Droplet from './droplet.js';
 
 export default class Rain {
   constructor() {
@@ -7,13 +7,13 @@ export default class Rain {
     this.height;
     this.canvas;
     this.ctx;
-    this.requestId
-    this.numberOfDroplets
+    this.requestId;
+    this.numberOfDroplets;
   }
 
   // create initial conavas element
   setup(width, height) {
-    this.canvas = document.getElementById('background')
+    this.canvas = document.getElementById('background');
     this.ctx = this.canvas.getContext('2d');
     this.width = width;
     this.height = height;
@@ -31,14 +31,14 @@ export default class Rain {
     this.ctx.clearRect(0, 0, this.width, this.height); // clear canvas
     this.drops.forEach(drop => {
       drop.fall();
-      let { x, y, len, thickness, color, lightness } = drop.show()
+      let { x, y, len, thickness, color, lightness } = drop.show();
       this.ctx.beginPath();
       this.ctx.lineWidth = thickness; // move to drop.js
       this.ctx.strokeStyle = `hsl(${color}, 100%, ${lightness}%)`; // move to drop.js
-      this.ctx.moveTo(x, y)
-      this.ctx.lineTo(x, len)
+      this.ctx.moveTo(x, y);
+      this.ctx.lineTo(x, len);
       this.ctx.stroke();
-    })
+    });
 
     this.requestId = window.requestAnimationFrame(this.draw.bind(this));
   }
@@ -46,8 +46,8 @@ export default class Rain {
   // cancels animation
   stopAnimation() {
     if (this.requestId) {
-       window.cancelAnimationFrame(this.requestId);
-       this.requestId = undefined;
+      window.cancelAnimationFrame(this.requestId);
+      this.requestId = undefined;
     }
   }
 }
